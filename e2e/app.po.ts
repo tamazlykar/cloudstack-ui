ea77f24e7bd8d482743e3eba2a89c7fa6cf968f2
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-08-17 14:29:11 +0700

    test ci

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class CloudstackUiPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getLogo() {
+    return element(by.css('.logo'));
   }
 }
