c8201d3b7fc07f92d541093ec40ac22cff801742
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-08-23 18:19:34 +0700

    update protractor conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,10 +9,7 @@ exports.config = {
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome',
-    'chromeOptions': {
-      'args': ['--headless', 'no-sandbox']
-    }
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
