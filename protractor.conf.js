c60413a78aa0e043aa26a650703270ae232471a8
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-08-24 11:09:07 +0700

    update travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: ['--headless', '--no-sandbox', '--disable-gpu']
+      args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=800x600']
     }
   },
   directConnect: true,
