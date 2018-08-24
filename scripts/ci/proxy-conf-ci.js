ca99e8d67811237964c2e75c51042f63df983784
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-08-24 14:11:45 +0700

    test e2e

--- scripts/ci/proxy-conf-ci.js
+++ scripts/ci/proxy-conf-ci.js
@@ -1,14 +1,60 @@
-const CLOUDSTACK_ENDPOINT = 'http://127.0.0.1:4222/';
+const CLOUDSTACK_ENDPOINT = 'https://cs2-1.netpoint-dc.com/';
+/*
+* Plugins endpoint
+* https://github.com/bwsw/cloudstack-ui#plugins-supported
+* */
+const PULSE_PLUGIN_ENDPOINT = 'http://192.168.1.218:8082/';
+const WEBSHELL_PLUGIN_ENDPOINT = 'http://192.168.1.218:8018/';
+
+function onProxyRes(proxyRes, req, res) {
+  var cookies = proxyRes.headers[ 'set-cookie' ];
+  var cookieRegex = /Secure/i;
+
+  if (cookies) {
+    var newCookie = cookies.map(function (cookie) {
+      if (cookieRegex.test(cookie)) {
+        return cookie.replace(cookieRegex, '');
+      }
+      return cookie;
+    });
+    delete proxyRes.headers[ 'set-cookie' ];
+    proxyRes.headers[ 'set-cookie' ] = newCookie;
+  }
+}
+
+const apiProxyConfig = {
+  context: [
+    '/client/api',
+    '/client/console'
+  ],
+  target: CLOUDSTACK_ENDPOINT,
+  secure: false
+};
+
+// If server works over https need to change Secure Cookie
+if (CLOUDSTACK_ENDPOINT.indexOf('https') === 0) {
+  apiProxyConfig.onProxyRes = onProxyRes;
+}
+
+const pulseProxyConfig = {
+  context: [ '/cs-extensions/pulse/**' ],
+  target: PULSE_PLUGIN_ENDPOINT,
+  secure: false,
+  pathRewrite: { '^/cs-extensions/pulse': '' }
+};
+
+const webShellProxyConfig = {
+  context: [ '/cs-extensions/webshell/**' ],
+  target: WEBSHELL_PLUGIN_ENDPOINT,
+  secure: false,
+  pathRewrite: { '^/cs-extensions/webshell': '' }
+};
 
 const PROXY_CONFIG = [
-  {
-    context: [
-      '/client/api',
-      '/client/console'
-    ],
-    target: CLOUDSTACK_ENDPOINT,
-    secure: false
-  }
+  apiProxyConfig,
+  pulseProxyConfig,
+  webShellProxyConfig
 ];
 
 module.exports = PROXY_CONFIG;
+
