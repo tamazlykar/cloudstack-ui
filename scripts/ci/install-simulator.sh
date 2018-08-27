e9c8cf2acd3855757a4e728fa635f20d050c1edc
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-08-27 08:34:33 +0700

    test ci

--- scripts/ci/install-simulator.sh
+++ scripts/ci/install-simulator.sh
@@ -14,10 +14,12 @@ for i in $(seq 1 50); do
   echo -en "\rChecking... ($i/50)";
   sleep 5;
 done
-echo -e "\rSimulator initialized successfully"
+echo "Simulator initialized successfully"
 
 sleep 5
 
 echo "Deploy Data Center started"
 docker exec cloudstack-simulator python /root/tools/marvin/marvin/deployDataCenter.py -i /root/setup/dev/basic.cfg
 echo "Data Center deployed"
+
+docker logs cloudstack-simulator
