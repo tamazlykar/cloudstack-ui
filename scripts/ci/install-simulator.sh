5a47a656718ffd63779494973c8ee638a2c69a31
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-08-27 12:01:16 +0700

    test ci

--- scripts/ci/install-simulator.sh
+++ scripts/ci/install-simulator.sh
@@ -6,13 +6,16 @@ docker run --name cloudstack-simulator -d -p 8888:8888 -p 4222:8080 tamazlykar/d
 echo "Docker container is started"
 
 echo "Wait until simulator initialized"
-for i in $(seq 1 100); do
+for i in $(seq 1 200); do
   PORT_STATUS=$(curl -LI 127.0.0.1:8888 -o /dev/null -w '%{http_code}\n' -s);
   echo $PORT_STATUS;
   if [ "$PORT_STATUS" = "403" ]; then
     echo -e "\nSimulator initialization is done";
     break;
   fi;
-  echo -en "\rChecking... ($i/100)";
+  if [ "$i" = "30" ]; then
+    docker ps -a
+  fi;
+  echo -en "\rChecking... ($i/200)";
   sleep 5;
 done
